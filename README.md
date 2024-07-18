e5880e88afcf106eb66d870742cbc6b01a154acd
jihoon098 <jihoon098@naver.com>
2024-07-18 16:42:38 +0900

    자동 구성(AutoConfiguration) 스터디 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,2 @@
+### SpringBoot  자동 구성 스터디 프로젝트
+dependency를 추가함으로써 boot가 어떤 조건에 의해서 어떤 자동 구성 빈들을 등록하는가? 에 대한 AutoConfiguraion 조건 결과 확인
---
+++ src/test/java/checkboot/springbootAc/SpringbootAcApplicationTests.java
@@ -0,0 +1,13 @@
+package checkboot.springbootAc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
